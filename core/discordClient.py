d11f922a870cddb3b94d9d26cb525f8f420ff8e6
HinamoreCh4n <motherrussia711@gmail.com>
2018-04-28 18:00:08 +0100

    Plugin system is fixed and works nominally currently only on_message event are supported

--- core/discordClient.py
+++ core/discordClient.py
@@ -9,6 +9,8 @@ logger = logging.getLogger(__name__)
 
 discord_client = discord.Client()
 
+main_channel_id = '402577355559796747'
+
 def get_token(file):
 
     with open(file, 'r') as f:
@@ -23,7 +25,8 @@ class DiscordBot():
        self.plugins = []
        self.plugin_manifest = json.load(open(f"{self.path}/plugin/manifest.json", "r"))
        self.manifests = []
-       self.plugin_exports = {}
+       self.plugin_exports = []
+       self.callback = {"on_message" : []}
        self._load_plugin_manifest()
 
     def _load_plugin_manifest(self):
@@ -38,24 +41,53 @@ class DiscordBot():
             spec = importlib.util.spec_from_file_location(temp['plugin'], f"{os.path.split(i)[0]}/{temp['plugin_object']}")
             plugin = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(plugin)
-            self.plugins.append({"plugin": f"{temp['plugin']}", "class": getattr(plugin, temp['plugin'])})
+            self.plugin_exports.append({"plugin": f"{temp['plugin']}", "class": getattr(plugin, temp['plugin'])})
+
+            for callable_function in temp["accepts"]:
+                self.callback[callable_function['on_event']].append({
+                    "command": callable_function['command'],
+                    "callback": callable_function['callback']})
+
             loaded_plugins.append(f"{temp['plugin']}@{temp['plugin_ver']}")
         return loaded_plugins
+
+    def find_callback(self, __type: str, prefix: str) -> str:
+        temp = self.callback[__type]
+        for command in temp:
+            if command['command'] == prefix:
+                return command['callback']
+            else:
+                pass
+        return "ERROR 5112"
+
+    def parse_callback(self, callback: str) -> object:
+        temp = callback.split('@')
+        print(self.plugin_exports)
+        for i in self.plugin_exports:
+            if i['plugin'] == temp[-1]:
+                return getattr(i['class'], temp[0])
+        return "5512"
+
 
 discord_bot = DiscordBot()
 
 @discord_client.event
 async def on_ready():
+    await discord_client.send_message(discord.Object(id=main_channel_id), f" Bot init ")
     logger.debug(discord_client.user.name)
     logger.debug(discord_client.user.id)
     logger.debug('------')
     for i in discord_bot._parse_plugin_manifests():
-        await discord_client.send_message(discord.Object(id='439598709299347456'), f"{i} ... OK")
+        await discord_client.send_message(discord.Object(id=main_channel_id), f"{i} ... OK")
 
 
 @discord_client.event
-async def on_message(self, message):
-    if message.content.startswith(prefix + 'ping'):
-        to_send = f"{str(message.timestamp - datetime.datetime.utcnow())} s"
-        await discord_client.send_message(message.channel, "Pong - Response time {to_send}")
-        print(message.timestamp - datetime.datetime.utcnow())
+async def on_message(message):
+    __type__ = "on_message"
+
+    callback = discord_bot.find_callback(__type__, message.content.split(' ')[0])
+    if callback != "5112":
+        to_call = discord_bot.parse_callback(callback)
+        if to_call != "5512":
+            await to_call(message.channel, discord_client)
+
--- plugin/test_plugin/__main__.py
+++ plugin/test_plugin/__main__.py
@@ -2,8 +2,8 @@ import os, sys
 import typing
 
 class TestPlugin:
-    def echo_func(discord_client: object) -> str:
+    def ping_func(discord_client: object) -> str:
         return "[TestPlugin] started __init__"
 
-    def ping_func(discord_client: object) -> str:
-        return "[TestPlugin] ping"
+    async def echo_func( message_channel, discord_client: object) -> str:
+        await discord_client.send_message(message_channel, "echo func works fine")

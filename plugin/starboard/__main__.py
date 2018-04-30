ae0f4451b2127c859314540468e30a8b8c99e806
Utku Pazarci <38797312+UtQ67@users.noreply.github.com>
2018-04-30 11:53:59 +0200

    Refactoredto new Plugin system

--- plugin/starboard/__main__.py
+++ plugin/starboard/__main__.py
@@ -3,32 +3,31 @@ import typing
 import discord
 import json
 
-class StarboardPlugin:
+class StarboardPlugin(object):
     """Starboard plugin"""
 
-    starboard_channel_id = ""
-    configs = {"min_stars" :[1], "accepted_emojis":["star", "star2"] }
+    def __init__(self):
+        self.starboard_channel_id = None
+        self.min_stars = 1
+        self.accepted_emoji = '⭐'
+        self.posted_message_id = []
 
     async def setStarboard(message, discord_client: object) -> str:
-        StarboardPlugin.starboard_channel_id = message.channel.id
+        self.starboard_channel_id = message.channel.id
         await discord_client.send_message(message.channel, "Starboard was set to this channel")
 
     async def on_reaction_add(reaction, user, discord_client: object):
-        await discord_client.send_message(reaction.message.channel, reaction.emoji)
-        if reaction.emoji in StarboardPlugin.configs["accepted_emojis"]:
-            reactionCnt = 0
-            # get all votes
-            temp = reaction.message.reactions
-            for r in temp:
-                if r.emoji.name in StarboardPlugin.configs["accepted_emojis"]:
-                    reactionCnt += r.count
-            if reactionCnt >= StarboardPlugin.configs["min_stars"]:
-                await StarboardPlugin.postToBoard(reaction.message)
-                print(reaction.message)
+        if reaction.message.id in posted_message_id:
+            return;
+        
+        if reaction.emoji == self.accepted_emoji:
+            if reaction.count >= self.min_stars:
+                await self.postToBoard(reaction.message)
+                posted_message_id.append(reaction.message.id)
 
     async def postToBoard(message, discord_client: object):
-        if StarboardPlugin.starboard_channel_id != "" :
-            await discord_client.send_message(client.get_channel(starboard_channel_id), message.content)
+        if self.starboard_channel_id != None :
+            await discord_client.send_message(client.get_channel(self.starboard_channel_id), message.content)
         
 
 

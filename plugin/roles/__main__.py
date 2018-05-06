853955b5dbd6474e2fabfa2bdc3801183b6a016d
obsunal <akirpi41@gmail.com>
2018-05-06 23:51:45 +0300

    Update __main__.py

--- plugin/roles/__main__.py
+++ plugin/roles/__main__.py
@@ -9,23 +9,6 @@ class RolesPlugin(object):
 		SelfRole_message_id = None
 		SelfRole_ids = []
 		SelfRole_reactions = []
-		config = {"RoleReactions":["emoji0", "emoji1", "emoji2", "emoji3"] "role0"= "rolenumber0", "role1"= "rolenumber1", "role2"= "rolenumber2", "role3"= "rolenumber3",}
-	async def setSelfRole(message, discord_client: object) -> str:
-		self.SelfRole_message_id = message.id
-		await discord_client.send_message(message.channel, "SelfRoles was set to" + reactionmessageid + "message")
-
-	async def setReaction (self,msg, rolenumber, roleid, reaction):
-		"""Syntax:  !setReaction 'integer' 'roleid' 'reactionstring' """
-		"""!setReaction 1 439568811822284801 :thanksjustin:"""
-		try:
-			self.rolenumber = message (13)
-			self.rolenumber = int(self.rolenumber)
-			self.SelfRole_ids  [self.rolenumber] = message (14:33)
-			self.SelfRole_ids = int(self.SelfRole_ids [rolenumber])
-			self.SelfRole_reaction [self.rolenumber] = message (35:end)
-		except ValueError:
-			await bot.say (message.channel,"Learn to use syntax, that's why i'll destroy world. (Syntax is !setReaction 'integer' 'roleid' 'reaction'")
-
 	async def on_reaction_add(reaction, user):
 		if reaction.message == self.SelfRole_message_id:
 			for x in xrange(len(self.SelfRole_reactions)):
